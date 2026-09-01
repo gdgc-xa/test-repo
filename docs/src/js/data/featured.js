08ae8c93a78fb39e30db7e936f8f7a5f55c5eb6f
gdgc-xa <gdsc@my.xu.edu.ph>
2026-09-01 12:47:56 +0800

    Update featured.js

--- docs/src/js/data/featured.js
+++ docs/src/js/data/featured.js
@@ -55,7 +55,7 @@
    ============================================================ */
 
 export const FEATURED = [
-  { id: 'gdgoc-xa', note: 'Example: Accepting Partnerships', cta: 'Example: Join now →' },
+  { id: 'gdgoc-xa', note: 'Example: Mu Accept ug Gwapa', cta: 'Join kung Gwapa ka' },
   {id: 'csg', note: 'Org sa Mga Gwapo', cta: 'ang di mujoin, bayot!'},
   // Add more featured orgs above this line, e.g.:
   // { id: 'jpia', note: 'New this term' },
